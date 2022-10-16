8117ee5055e83f7bc75a6a99df745dedd64beae8
YujiTakebuchi <takeyou.swift@gmail.com>
2022-10-16 22:06:25 +0900

    Add color list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import IngredientList from "./IngredientsList";
 import Instructions from "./Instructions";
 import StarRating from "./StarRating";
+import ColorList from "./ColorList";
+import colorData from "../../data/color-data.json";
 
 function Recipe({ name, ingredients, steps }) {
+    const [colors, setColors] = useState(colorData);
     return (
         <section id={name.toLowerCase().replace(/ /g, "-")}>
             <h1>{name}</h1>
             <IngredientList list={ingredients} />
             <Instructions title="Cooking Instructions" steps={steps} />
-            <StarRating />
+            <StarRating
+                style={{ backgroundColor: "lightblue" }}
+                onDoubleClick={e => alert("double click!")}
+            />
+            <ColorList
+                colors={colors}
+                onRemoveColor={id => {
+                    const newColors = colors.filter(color => color.id !== id);
+                    setColors(newColors);
+                }}
+                onRateColor={(id, rating) => {
+                    const newColors = colors.map(color => 
+                        color.id === id ? { ...color, rating } : color
+                    );
+                    setColors(newColors);
+                }}
+            />
         </section>
     );
 }
--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -5,22 +5,43 @@ const Star = ({ selected = false, onSelect = f => f }) => (
     <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 )
 
-export default function StarRating({ style={}, totalStars = 5, ...props }) {
-    const [selectedStars, setSelectedStars] = useState(0);
+export default function StarRating({
+        totalStars = 5,
+        selectedStars = 0,
+        onRate = f => f
+    }) {
     return (
-        <div style={{ padding: "5px", ...style }} {...props}>
-            {
-                [...Array(totalStars).fill()].map((n, i) => (
-                    <Star
-                        key={i}
-                        selected={selectedStars > i}
-                        onSelect={() => setSelectedStars(i + 1)}
-                    />
-                ))
-            }
+        <>
+            {[...Array(totalStars).fill()].map((n, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => onRate(i + 1)}
+                />
+            ))}
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
-        </div>
+        </>
     );
 }
+
+// export default function StarRating({ style={}, totalStars = 5, ...props }) {
+//     const [selectedStars, setSelectedStars] = useState(0);
+//     return (
+//         <div style={{ padding: 5, ...style }} {...props}>
+//             {
+//                 [...Array(totalStars).fill()].map((n, i) => (
+//                     <Star
+//                         key={i}
+//                         selected={selectedStars > i}
+//                         onSelect={() => setSelectedStars(i + 1)}
+//                     />
+//                 ))
+//             }
+//             <p>
+//                 {selectedStars} of {totalStars} stars
+//             </p>
+//         </div>
+//     );
+// }
